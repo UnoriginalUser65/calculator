5e5f762ce189b50f18a738cd867be2af44721eb7
UnoriginalUser65 <philipsalt6500@gmail.com>
2025-07-09 00:16:39 -0400

    exception handling for negative square roots, locking operation while an operation has already been pressed, display for the reciprocal, square, and square root operations. Added numpad compatibility

--- calculator/obj/Debug/net8.0-windows/calculator.AssemblyInfo.cs
+++ calculator/obj/Debug/net8.0-windows/calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40332b1a049142efa9f49c8c37ba8cf87d32c24")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
